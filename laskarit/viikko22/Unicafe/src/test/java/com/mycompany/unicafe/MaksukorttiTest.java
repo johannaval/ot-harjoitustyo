1ec62d75477b85b45a2dad23ad97e14bb4ddc8c3
johannaval <johanna.valminen@gmail.com>
2020-03-16 11:32:21 +0200

    viikko2 maksukortti ja kassapääte jacoco testit 100%

--- laskarit/viikko22/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko22/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -8,6 +8,7 @@ public class MaksukorttiTest {
 
     Maksukortti kortti;
 
+
     @Before
     public void setUp() {
         kortti = new Maksukortti(10);
@@ -15,6 +16,54 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
+    }
+
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+
+        kortti.lataaRahaa(50);
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 0.70", kortti.toString());
+    }
+
+    @Test
+    public void saldoVaheneeOikeinKunSaldoaTarpeeksi() {
+
+        kortti.otaRahaa(7);
+        assertEquals("saldo: 0.3", kortti.toString());
+    }
+
+    @Test
+    public void saldoEiMuutuJosRahaaEiTarpeeksi() {
+
+        kortti.otaRahaa(11);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+
+    @Test
+    public void palauttaaTrueJosOnnistuu() {
+
+        assertEquals(true, kortti.otaRahaa(5));
+    }
+
+    @Test
+    public void palauttaaFalseJosEpaonnistuu() {
+
+        assertEquals(false, kortti.otaRahaa(15));
+    }
+
+    @Test
+    public void saldoPalauttaaOikean() {
+
+        kortti.lataaRahaa(5);
+        int saldo = kortti.saldo();
+        assertEquals(15.0, saldo,15.0);
     }
 }
+
